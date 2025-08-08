9973ef6f8e2f6d4a895e93fc7d5940c4aeeffe5f
WorldofShadows <100038485+WorldofShadows@users.noreply.github.com>
2025-08-08 10:00:37 +0200

    added gameloop, clock tickrate and circleshape

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from constants import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,7 +19,7 @@ def main():
 
         screen.fill((0,0,0))
         pygame.display.flip()
-   
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
